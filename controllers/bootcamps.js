50d712ffc649854d09123de513bbf47588312dac
Skelman50 <skelman50@gmail.com>
2019-10-28 15:48:10 +0200

    add select and sort

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,9 +1,27 @@
 const Bootcamp = require("../models/Bootcamp");
 const ErrorResponse = require("../utils/error-response");
 const asyncHandler = require("express-async-handler");
+const { geocoder } = require("../utils/geocoder");
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  const reqQuery = { ...req.query };
+  const removeFields = ["select", "sort"];
+  removeFields.forEach(param => delete reqQuery[param]);
+  let queryStr = JSON.stringify(reqQuery);
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, math => `$${math}`);
+  let query = Bootcamp.find(JSON.parse(queryStr));
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query.select(fields);
+  }
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query.sort(sortBy);
+  } else {
+    query.sort("-createdAt");
+  }
+  const bootcamps = await query;
   res.status(200).json({ succes: true, data: bootcamps });
 });
 
@@ -48,4 +66,19 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse("Bootcamp not found", 404));
   }
   res.status(200).json({ succes: true, data: bootcamp });
+});
+
+exports.getBootcampsInradius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  const loc = await geocoder.geocode(zipcode);
+  const [{ latitude, longitude }] = loc;
+
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[longitude, latitude], radius] } }
+  });
+
+  res.json({ succes: true, data: bootcamps });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,24 +1,27 @@
-const express = require('express');
+const express = require("express");
 
 const {
   getBootcamps,
   getBootcamp,
   createBootcamp,
   updateBootcamp,
-  deleteBootcamp
-} = require('../controllers/bootcamps');
+  deleteBootcamp,
+  getBootcampsInradius
+} = require("../controllers/bootcamps");
 
 const router = express.Router();
 
 router
-  .route('/')
+  .route("/")
   .get(getBootcamps)
   .post(createBootcamp);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(getBootcamp)
   .put(updateBootcamp)
   .delete(deleteBootcamp);
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInradius);
 
 module.exports = router;
